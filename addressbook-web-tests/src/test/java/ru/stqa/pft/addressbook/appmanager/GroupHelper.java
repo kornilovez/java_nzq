cb1c104433a4db336ef50d04f21c5ad45dcd29b2
kornilovez <59286998+kornilovez@users.noreply.github.com>
2021-06-17 13:25:24 +0300

    тест изменение группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -36,4 +36,12 @@ public class GroupHelper extends HelperBase {
     public void selectGroup() {
         click(By.name("selected[]"));
     }
+
+    public void initGroupModification() {
+        click(By.name("edit"));
+    }
+
+    public void submitGroupModofication() {
+        click(By.name("update"));
+    }
 }
